6f272a224e5b570f2fe44947afe0cbc4f5d3b21f
IMHOJEONG <39ghwjd@naver.com>
2022-01-31 15:09:49 +0900

    chore: 테스트 추가(책 4.4) 완료 - beforeEach 추가

--- test/chap04/province.test.js
+++ test/chap04/province.test.js
@@ -3,17 +3,18 @@ import Province from '../../chap04/Province';
 import { expect } from 'chai';
 
 describe('province', ()=>{
+    let asia;
+    beforeEach(()=>{
+        asia = new Province(sampleProvinceData());
+    })
+    
     it('shortfall', ()=>{
 
-        // 1) fixture 설정 
-        const asia = new Province(sampleProvinceData());
-
-        // 2) 검증
         expect(asia.shortfall).equal(5);
     });
     
     it('profit', ()=>{
-        const asia = new Province(sampleProvinceData());
+        
         expect(asia.profit).equal(230);
     })
 });
