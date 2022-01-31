c233f3326f52a654d1f848fc161ca4a24d9d21ec
IMHOJEONG <39ghwjd@naver.com>
2022-02-01 01:17:36 +0900

    chore: 1.6 - 계산 단계와 포맷팅 단계 분리하기 - createStatementData.js 파일 생성 및 수정

--- chap01/statement.js
+++ chap01/statement.js
@@ -1,8 +1,9 @@
 /*
 * 리팩토링을 하고 나면, 반드시 테스트를 진행한다.
 */
+import createStatementData from "./createStatementData";
 
-function renderPlainText(data, plays) {
+function renderPlainText(data) {
     
     let result = `청구 내역 (고객명: ${data.customer})\n`;
     
@@ -32,70 +33,6 @@ function renderPlainText(data, plays) {
 
 export default function statement(invoice, plays) {
     
-    const statementData = {};
-    statementData.customer = invoice.customer;
-    statementData.performances = invoice.performances.map(enrichPerformance);
-    statementData.totalAmount = totalAmount(statementData);
-    statementData.totalVolumeCredits = totalVolumeCredits(statementData);
-    return renderPlainText(statementData, plays);  
+    return renderPlainText(createStatementData(invoice, plays));  
     
-    // 임시 변수를 최대한 제거 => 로컬 범위에 존재하는 이름이 늘어나서 추출 작업이 복잡해짐 
-    function enrichPerformance(aPerformance){
-        const result = Object.assign({}, aPerformance);
-        result.play = playFor(result);
-        result.amount = amountFor(result);
-        result.volumeCredits = volumeCreditsFor(result);
-        return result;
-    }    
-    function playFor(aPerformance) {
-        return plays[aPerformance.playID];
-    }
-    
-    function amountFor(aPerformance){
-        let result = 0;
-        
-        switch ( aPerformance.play.type) {
-            case "tragedy":
-                result = 40000;
-                if(aPerformance.audience > 30){
-                result += 1000 * (aPerformance.audience - 30);
-            }
-            break;
-            case "comedy": 
-            result = 30000;
-            if(aPerformance.audience > 20){
-                result += 10000 + 500 * (aPerformance.audience - 20);
-            } 
-            result += 300 * aPerformance.audience;
-            break;
-            default:
-                throw new Error(`알 수 없는 장르: ${aPerformance.play.type}`);
-            }
-            
-            return result; // 함수 안에서 값이 바뀌는 변수 반환
-            
-    }
-    function volumeCreditsFor(aPerformance) {
-        let result = 0;
-        result += Math.max(aPerformance.audience - 30, 0);
-        
-        if ("comedy" === aPerformance.play.type) {
-            result += Math.floor(aPerformance.audience / 5);
-        }
-        
-        return result;
-    }    
-    
-    function totalAmount(data) {
-            
-
-        return data.performances.reduce((total, p) => total+=p.amount, 0);
-
-    }
-
-    function totalVolumeCredits(data) {
-
-        return data.performances.reduce((total, p) => total+=p.volumeCredits, 0);
-
-    }
 }
