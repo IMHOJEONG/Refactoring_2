f530cfb271d8fcd47fbaf5bfca75d2c69d093573
IMHOJEONG <39ghwjd@naver.com>
2022-02-01 01:25:06 +0900

    chore: 1.6 - 계산 단계와 포맷팅 단계 분리하기 - HTML 버전 함수 작성

--- chap01/statement.js
+++ chap01/statement.js
@@ -3,6 +3,14 @@
 */
 import createStatementData from "./createStatementData";
 
+function usd(aNumber) {
+    return new Intl.NumberFormat("en-US",
+    {
+        style: "currency", currency: "USD",
+        minimumFractionDigits: 2
+    }).format(aNumber/100);
+}
+
 function renderPlainText(data) {
     
     let result = `청구 내역 (고객명: ${data.customer})\n`;
@@ -18,18 +26,31 @@ function renderPlainText(data) {
     result += `총액: ${usd(data.totalAmount)}\n`;
     result += `적립 포인트: ${data.totalVolumeCredits}점\n`;
     return result;
-    
-    
-    
-    function usd(aNumber) {
-        return new Intl.NumberFormat("en-US",
-        {
-            style: "currency", currency: "USD",
-            minimumFractionDigits: 2
-        }).format(aNumber/100);
-    }
-    
+
 }
+
+// HTML 버전 작성 준비
+function htmlStatement(invoice, plays){
+    return renderHtml(createStatementData(invoice, plays));
+}
+
+function renderHTML(data) {
+    let result = `<h1>청구 내역 (고객명: ${data.customer})</h1>\n`;
+    result += "<table>\n";
+    result += "<tr><th>연극</th><th>좌석 수</th><th>금액</th></tr>";
+    for(let perf of data.performances){
+        result+=`  <tr><td>${perf.play.name}</td><td>(${perf.audience}석)</td>`;
+        result+=`<td>${usd(perf.amount)}</td></tr>\n`;
+    }
+
+    result+= "</table>\n";
+    result+=`<p>총액: <em>${usd(data.totalAmount)}</em></p>\n`;
+    result+=`<p>적립 포인트: <em>${data.totalVolumeCredits}</em>점</p>\n`;
+    return result;
+
+}
+
+
 
 export default function statement(invoice, plays) {
     
