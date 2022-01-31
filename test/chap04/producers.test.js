c1ce2ea57e1109c335376916a89e60103e529d61
IMHOJEONG <39ghwjd@naver.com>
2022-01-31 15:42:01 +0900

    chore: 경계조건 검사하기(책 4.6) - 에러와 실패의 차이

--- test/chap04/producers.test.js
+++ test/chap04/producers.test.js
@@ -21,3 +21,17 @@ describe('no producers', ()=>{
         expect(noProducers.profit).equal(0);
     })
 })
+
+describe('생산자 수 필드에 문자열을 대입한다(string for producers)', ()=>{
+    it('', ()=>{
+        const data = {
+            name: "String producers",
+            producers: "",
+            demand: 30,
+            price: 20
+        };
+
+        const prov = new Province(data);
+        expect(prov.shortfall).equal(0);
+    })
+})
