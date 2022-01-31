bd41118677488e146c1bd790ebee6f0291bed028
IMHOJEONG <39ghwjd@naver.com>
2022-01-31 23:05:12 +0900

    chore: 변수 inline 후 함수 이름 변경

--- chap01/statement.js
+++ chap01/statement.js
@@ -42,14 +42,13 @@ export default function statement(invoice, plays) {
         
     }
     
-    let totalAmount = appleSauce();
     
-    result += `총액: ${usd(totalAmount)}\n`;
+    result += `총액: ${usd(totalAmount())}\n`;
     result += `적립 포인트: ${totalVolumeCredits()}점\n`;
     return result;
 
 
-    function appleSauce() {
+    function totalAmount() {
 
         let totalAmount = 0;
         for (let perf of invoice.performances) {
