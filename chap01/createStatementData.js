0a5d6181113e9ceabc34128f60b061ad3438ace3
IMHOJEONG <39ghwjd@naver.com>
2022-02-01 12:27:19 +0900

    chore: 1.8 - 다형성을 활용한 계산 코드 재구성하기 - 일반적인 경우를 기본으로 삼아 슈퍼클래스에 남겨두고, 장르마다 달라지는 부분은 필요할 때 override하게 만드는 것

--- chap01/createStatementData.js
+++ chap01/createStatementData.js
@@ -5,35 +5,11 @@ class PerformanceCalculator {
     }
 
     get amount(){
-        let result = 0;
+        throw new Error(`서브클래스에서 처리하도록 설계되었습니다.`)
         
-        switch ( this.play.type) {
-            case "tragedy":
-                throw `오류 발생`;
-            
-            case "comedy": 
-            result = 30000;
-            if(this.performance.audience > 20){
-                result += 10000 + 500 * (this.performance.audience - 20);
-            } 
-            result += 300 * this.performance.audience;
-            break;
-            default:
-                throw new Error(`알 수 없는 장르: ${this.play.type}`);
-            }
-            
-            return result; // 함수 안에서 값이 바뀌는 변수 반환
-            
     }
     get volumeCredits() {
-        let result = 0;
-        result += Math.max(this.performance.audience - 30, 0);
-        
-        if ("comedy" === this.play.type) {
-            result += Math.floor(this.performance.audience / 5);
-        }
-        
-        return result;
+        return Math.max(this.performance.audience - 30, 0);
     }    
 }
 
@@ -48,7 +24,6 @@ function createPerformanceCalculator(aPerformance, aPlay) {
             throw new Error(`알 수 없는 장르: ${aPlay.type}`);
     }
 
-    return new PerformanceCalculator(aPerformance, aPlay);
 }
 
 class TragedyCalculator extends PerformanceCalculator {
@@ -61,10 +36,25 @@ class TragedyCalculator extends PerformanceCalculator {
         }
         return result;
     }
+
+    get volumeCredits() {
+        return Math.max(this.performance.audience - 30, 0);
+    }
 }
 
 class ComedyCalculator extends PerformanceCalculator {
+    get amount(){
+        let result = 30000;
+        if(this.performance.audience > 20){
+            result += 10000 + 500 * (this.performance.audience - 20);
+        } 
+        result += 300 * this.performance.audience;
+        return result;
+    }
 
+    get volumeCredits() {
+        return super.volumeCredits + Math.floor(this.performance.audience / 5);
+    }
 }
 
 export default function createStatementData(invoice, plays) {
