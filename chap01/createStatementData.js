ee6b9bd3e96b442557086eff30b21ff6c0a2dc46
IMHOJEONG <39ghwjd@naver.com>
2022-02-01 12:08:37 +0900

    chore: 1.8 - 다형성을 활용한 계산 코드 재구성하기 - 같은 방법으로 적립 포인트를 계산하는 함수도 이동

--- chap01/createStatementData.js
+++ chap01/createStatementData.js
@@ -28,7 +28,16 @@ class PerformanceCalculator {
             return result; // 함수 안에서 값이 바뀌는 변수 반환
             
     }
-    
+    get volumeCredits() {
+        let result = 0;
+        result += Math.max(this.performance.audience - 30, 0);
+        
+        if ("comedy" === this.play.type) {
+            result += Math.floor(this.performance.audience / 5);
+        }
+        
+        return result;
+    }    
 }
 
 export default function createStatementData(invoice, plays) {
@@ -44,7 +53,7 @@ export default function createStatementData(invoice, plays) {
         const result = Object.assign({}, aPerformance);
         result.play = calculator.play;
         result.amount = calculator.amount
-        result.volumeCredits = volumeCreditsFor(result);
+        result.volumeCredits = calculator.volumeCredits;
         return result;
     }    
     function playFor(aPerformance) {
